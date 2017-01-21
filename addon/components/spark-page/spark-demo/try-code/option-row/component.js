2426bf0e94d65fec33c35661782f7da24d8a1b7b
Tristan Edwards <tristan.edwards@me.com>
2017-01-21 13:08:26 +0100

    Add boolean toggle for demo code

--- addon/components/spark-page/spark-demo/try-code/option-row/component.js
+++ addon/components/spark-page/spark-demo/try-code/option-row/component.js
@@ -9,12 +9,28 @@ export default Ember.Component.extend({
 
   localClassNames: 'option',
 
+  typeOfValue: Ember.computed(function() {
+    let value = this.get('value');
+    return typeof value;
+  }),
+
+  valueIsBoolean: Ember.computed('typeOfValue', function() {
+    let type = this.get('typeOfValue');
+    return type === "boolean";
+  }),
+
   valueDidChange: Ember.observer('value', function() {
     let key = this.get('key');
     let value = this.get('value');
 
     this.attrs.didUpdate(key, value);
   }),
+
+  actions: {
+    toggleBoolValue() {
+      this.toggleProperty('value');
+    },
+  },
 
 });
 
