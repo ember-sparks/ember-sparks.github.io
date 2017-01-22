3c5836fd2b1b3236b1359d104ce867549114f47e
Tristan Edwards <tristan.edwards@me.com>
2017-01-22 21:29:03 +0100

    Remove 404 symlink

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -5,7 +5,6 @@ ember github-pages:commit --branch master --message "Deploy gh-pages from commit
 
 # Add Symlinks:
 git checkout master;
-ln -s index.html 404.html;
 ln -s index.html docs.html;
 git add .;
 git commit -m "Re-add symlinks";
