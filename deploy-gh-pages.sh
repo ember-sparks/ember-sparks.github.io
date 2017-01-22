fb6981e45fb0651a028874abf8463bb5d45f5526
Tristan Edwards <tristan.edwards@me.com>
2017-01-22 21:17:39 +0100

    New symlink script

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -xe
+
+ember github-pages:commit --branch master --message \"Deploy gh-pages from commit $(git rev-parse HEAD)\";
+
+# Add Symlinks:
+git checkout master;
+ln -s index.html 404.html;
+ln -s index.html docs.html;
+git add .;
+git commit -m "Re-add symlinks";
+git push -u origin master;
+git checkout ember;
+
